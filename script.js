a063a5ab0a113959d2e7fdfdd4f7cfe854707808
Joykiran0318 <joykiran0322@gmail.com>
2025-09-03 18:49:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleTheme() {
+    const body = document.body;
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    }
+}
